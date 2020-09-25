df785c7b62938e1f1a400f82daa455af7ab0669d
marijanabo <bogojevicmarijana1@gmail.com>
2020-09-25 11:25:15 +0200

    Updates

---
+++ precache-manifest.4adb9f96d26ce54b8d018a67b5cf3075.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4df34a3215287d948c7eb158b44e21f",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "97f29d13c0dd8433787a",
+    "url": "/recipes/static/css/main.3701d256.chunk.css"
+  },
+  {
+    "revision": "36dfcca5377da0a6ebfa",
+    "url": "/recipes/static/js/2.05bf1100.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipes/static/js/2.05bf1100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f29d13c0dd8433787a",
+    "url": "/recipes/static/js/main.cc9be572.chunk.js"
+  },
+  {
+    "revision": "4192243a29b7d3d38f0e",
+    "url": "/recipes/static/js/runtime-main.f2d2de19.js"
+  },
+  {
+    "revision": "359da22031d8335655a8cd93fc979eab",
+    "url": "/recipes/static/media/DMSerifDisplay-Regular.359da220.ttf"
+  }
+]);
